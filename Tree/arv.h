d89672290d32601e359e554f4258b2b3b8f99e77
amandasantos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2026-03-13 12:35:54 -0300

    funções novas

--- Tree/arv.h
+++ Tree/arv.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define fmt "%d"
+#define fmt "%d "
 typedef int Item;
 typedef struct arv {
     struct arv *esq;
@@ -44,9 +45,8 @@ void posOrdem(Arv A) {
 
 //Total de nós:
 int nos(Arv A) {
-    if (A == NULL) {
-        return 0;
-    }
+    if (A == NULL) return 0;
+
     return 1 + nos(A->esq) + nos(A->dir);
 }
 
@@ -61,6 +61,16 @@ int folhas(Arv A) {
     return folhas(A->esq) + folhas(A->dir);
 }
 
+//Altura de uma árvore:
+int altura(Arv A) {
+    if (A == NULL) return 0;
+
+    int h_esq = altura(A->esq);
+    int h_dir = altura(A->dir);
+
+    return 1 + (h_esq > h_dir ? h_esq : h_dir);
+}
+
 //Destruição de árvore binária:
 void destroi(Arv *A) {
     if (A == NULL) return;
@@ -70,14 +80,16 @@ void destroi(Arv *A) {
     *A = NULL;
 }
 
-//Inserção em árvore de busca binária:
+//ÁRVORE DE BUSCA BINÁRIA
+
+//Inserção:
 void ins(Item x, Arv *A) {
     if (*A == NULL) *A = arv(NULL, x, NULL);
     else if (x <= (*A)->item) ins(x, &(*A)->esq);
     else ins(x, &(*A)->dir);
 }
 
-//Busca em árvore de busca binária:
+//Busca:
 int busca(Item x, Arv A) {
     if (A == NULL) return 0; 
     if (x == A->item) return 1;
@@ -94,27 +106,10 @@ int tem(Arv A, Item x) {
     return tem(A->esq, x) || tem(A->dir, x);
 }
 
-//Estritamente binária:
-int eb(Arv A) {
-    //Árvore vazia é estritamente binária
-    if (A == NULL) return 1;
-
-    // Se for uma folha (zero filhos), é tbm
-    if (A->esq == NULL && A->dir == NULL) return 1;
-
-    // Se tiver os dois filhos, precisamos verificar se os filhos também são estritamente binários
-    if (A->esq != NULL && A->dir != NULL) {
-        return eb(A->esq) && eb(A->dir);
-    }
-}
-
-
-
-
-//Remoção em árvore de busca binária:
+//Remoção:
 Item remMax(Arv *A) {
     if (*A == NULL) abort();
-    while ( (*A)->dir != NULL) A = &(*A)->dir;
+    while ((*A)->dir != NULL) A = &(*A)->dir;
     Arv n = *A;
     Item x = n->item;
     *A = n->esq;
@@ -135,4 +130,78 @@ void rem(Item  x, Arv *A) {
     }
     else if (x <= (*A)->item) rem(x, &(*A)->esq);
     else rem(x, &(*A)->dir);
+}
+
+//Estritamente binária:
+int eb(Arv A) {
+    //Árvore vazia é estritamente binária
+    if (A == NULL) return 1;
+
+    // Se for uma folha (zero filhos), é tbm
+    if (A->esq == NULL && A->dir == NULL) return 1;
+
+    // Se tiver os dois filhos, precisamos verificar se os filhos também são estritamente binários
+    if (A->esq != NULL && A->dir != NULL) {
+        return eb(A->esq) && eb(A->dir);
+    }
+}
+
+void ehCheia(Arv A) {
+    if (A == NULL){
+        printf("\nA arvore esta vazia e cheia por definicao!\n");
+    }
+
+    int h = altura(A);
+    int n = nos(A);
+
+    if (n == pow(2, h) - 1) {
+        printf("\nA arvore eh cheia!");
+    } else {
+        printf("\nA arvore nao eh cheia!");
+    }
+}
+
+void ehCompleta(Arv A) {
+    if (A == NULL) {
+        printf("\nA arvore esta vazia e completa por definicao!\n");
+    }
+
+    int n = nos(A);
+    int h_real = altura(A);
+
+    int h_min = 1 + floor(log2(n));
+
+    if (h_real == h_min) {
+        printf("\nA arvore eh completa!");
+    } else {
+        printf("\nA arvore nao eh completa!");
+    }
+}
+
+void remFusao(Item x, Arv *A) {
+
+    if (*A == NULL) return;
+
+    if (x < (*A)->item) remFusao(x, &(*A)->esq);
+
+    else if (x > (*A)->item) remFusao(x, &(*A)->dir);
+
+    else {
+        Arv n = *A;
+
+        if (n->esq == NULL) *A = n->dir;
+
+        else if (n->dir == NULL) *A = n->esq;
+
+        else {
+            Arv p = n->esq;
+
+            while (p->dir != NULL)
+                p = p->dir;
+
+            p->dir = n->dir;
+            *A = n->esq;
+        }
+        free(n);
+    }
 }
